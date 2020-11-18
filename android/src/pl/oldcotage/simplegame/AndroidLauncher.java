2f318b6f55ca2d66072e9db8ef8122d9bd781536
Adrian <adrian.pelka@hotmail.com>
2020-11-18 20:10:27 +0100

    Init

---
+++ android/src/pl/oldcotage/simplegame/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package pl.oldcotage.simplegame;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import pl.oldcotage.simplegame.GameRunner;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new GameRunner(), config);
+	}
+}
---
+++ desktop/src/pl/oldcotage/simplegame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package pl.oldcotage.simplegame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import pl.oldcotage.simplegame.GameRunner;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameRunner(), config);
+	}
+}
