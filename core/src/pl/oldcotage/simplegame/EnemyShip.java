905d290e13a0daf049bc2ea4ea485c0b0c11399a
PrzemekBus <54452812+PrzemekBus@users.noreply.github.com>
2020-11-21 22:56:22 +0100

    created Player ship class with fire lasers method

---
+++ core/src/pl/oldcotage/simplegame/EnemyShip.java
@@ -0,0 +1,4 @@
+package pl.oldcotage.simplegame;
+
+public class EnemyShip {
+}
--- core/src/pl/oldcotage/simplegame/PlayerShip.java
+++ core/src/pl/oldcotage/simplegame/PlayerShip.java
@@ -1,4 +1,35 @@
 package pl.oldcotage.simplegame;
 
-public class PlayerShip {
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class PlayerShip extends Ship {
+
+
+    public PlayerShip(float xCentre, float yCentre,
+                      float width, float height,
+                      float movementSpeed, int hitPoints,
+                      float laserWidth, float laserHeight,
+                      float laserMovementSpeed, float timeBetweenShoots,
+                      TextureRegion shipTextureRegion,
+                      TextureRegion laserTextureRegion) {
+
+        super(xCentre, yCentre,
+                width, height,
+                movementSpeed, hitPoints,
+                laserWidth, laserHeight,
+                laserMovementSpeed, timeBetweenShoots,
+                shipTextureRegion,
+                laserTextureRegion);
+    }
+
+    @Override
+    public Laser[] fireLasers() {
+        Laser[] laser = new Laser[2];
+        laser[0] = new Laser(xPosition+width*0.07f,yPosition+height*0.45f,laserWidth,laserHeight,laserTextureRegion,laserMovementSpeed);
+        laser[1] = new Laser(xPosition+width*0.93f,yPosition+height*0.45f,laserWidth,laserHeight,laserTextureRegion,laserMovementSpeed);
+
+        timeSinceLastShot = 0;
+
+        return laser;
+    }
 }
